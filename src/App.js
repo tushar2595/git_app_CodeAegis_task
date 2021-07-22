750b9226dddec9025bd45e3e851fbb27d0a81cca
Tushar <tushardhawan25@gmail.com>
2021-07-22 21:09:19 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ListUsers from "./components/list/Listusers";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={ListUsers} />
+          </Switch>
+        </Router>
+      </Provider>
+    )
+  }
 }
-
 export default App;
---
+++ src/components/list/Listusers.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { connect } from 'react-redux'
+import { fetchUsers } from '../../redux/usersActions';
+import "./style.css";
+const ListUsers = ({ userData, fetchUsers, loading }) => {
+    console.log(userData.loading, 'lllllllllll');
+    const [gitRepo, setgitRepo] = useState([]);
+    const [searchValue, setSearchValue] = useState("");
+    useEffect(() => fetchUsers(), []);
+    const showRepo = (data) => {
+        console.log(data);
+        setgitRepo(data);
+    }
+    return <div className={"list-container"}>
+        <h2>CodeAegis Task</h2>
+        <div>
+            <p>Please Enter name of the users</p>
+            <input className={"search-input"} placeholder={"Enter username"} onChange={(e) => { setSearchValue(e.target.value) }} />
+        </div>
+        <div>
+            Users Repo URL :-
+            <p style={{ border: "2px solid red", padding: "5px" }}>
+                {gitRepo}
+            </p>
+        </div>
+
+        <div>
+            <h3>Git hub users List</h3>
+        </div>
+        {
+            userData.loading ?
+                <p>Loading please wait...</p> : ""
+        }
+        {
+            userData && userData.users.filter((data => data.login.startsWith(searchValue))).map((data) => {
+                return (
+                    <div>
+                        <p style={{ cursor: "pointer" }} onClick={() => { showRepo(data.repos_url) }}>
+                            {data.login.toUpperCase()}
+                        </p>
+                    </div>
+                )
+            })
+        }
+
+
+    </div>;
+};
+const mapStateToProps = (state) => {
+    return {
+        userData: state.user,
+        loading: state.loading
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchUsers: () => dispatch(fetchUsers()),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ListUsers);
